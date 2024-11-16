7b4dfd12afb8b68d605140cf99fc2b006ada8d62
Kashish Juneja <69794677+KashishJuneja101003@users.noreply.github.com>
2024-11-16 19:00:40 +0530

    liver done

--- liver/doctors/doctor_details.js
+++
@@ -1,4 +0,0 @@
-document.getElementById("book-appointment").addEventListener("click", function () {
-    // Redirect to booking confirmation page
-    window.location.href = "appointment_confirmation.html";
-});
